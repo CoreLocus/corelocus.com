0f99bfe2ca92c35764282941f07401021d09f6a4
Jessie Amaral <59110906+CoreLocus@users.noreply.github.com>
2019-12-22 05:15:48 -0500

    Master commit update (lots of changes)

--- assets/js/glyphs.js
+++ assets/js/glyphs.js
@@ -1,6 +1,7 @@
 /* Page loading animation */
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('loader'),
+    /* Note: Paths are handled at the page level. This is why the path references the parent assets folder */
   path: 'assets/js/vectors/loader.json',
   renderer: 'svg',
   loop: true,
