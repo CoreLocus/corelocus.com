7d81964fe3775aebdba6efecf95977e5203946da
Jessie Amaral <59110906+CoreLocus@users.noreply.github.com>
2019-12-22 04:13:30 -0500

    Add preloader and jquery transitions

--- glyphs.js
+++ glyphs.js
@@ -1,5 +1,5 @@
 var animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie'),
+  container: document.getElementById('loader'),
   path: 'data.json',
   renderer: 'svg',
   loop: true,
