943cb92aa6d2aa94b69a3420e3724bbc5bdf4cc4
Jessie Amaral <59110906+CoreLocus@users.noreply.github.com>
2019-12-26 04:07:45 -0500

    Use ID instead of CLASS

--- assets/js/cover.js
+++ assets/js/cover.js
@@ -1,7 +1,6 @@
 /* Cover Animation */
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('coveranim'),
-    /* Note: Paths are handled at the page level. This is why the path references the parent assets folder */
   path: 'assets/js/vectors/homepage-cover.json',
   renderer: 'svg',
   loop: true,
