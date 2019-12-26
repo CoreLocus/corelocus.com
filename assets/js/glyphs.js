e2d7e8caf54cfff89b325aac9b00cf7df0cfd7ca
Jessie Amaral <59110906+CoreLocus@users.noreply.github.com>
2019-12-26 03:51:44 -0500

    Test cover

--- assets/js/glyphs.js
+++ assets/js/glyphs.js
@@ -7,3 +7,13 @@ var animation = bodymovin.loadAnimation({
   loop: true,
   autoplay: true
 })
+
+/* Cover Animation */
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('coveranim'),
+    /* Note: Paths are handled at the page level. This is why the path references the parent assets folder */
+  path: 'assets/js/vectors/homepage-cover.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true
+})
